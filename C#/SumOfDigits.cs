ec945480345f444ee9a732cb1385b8ad3b7f63d7
samiksha144 <samikshagavand73@gmail.com>
2024-02-23 19:15:49 +0530

    Sum of digits

---
+++ C#/SumOfDigits.cs
@@ -0,0 +1,24 @@
+//Write a c# program to print sum of digits.
+
+
+using System;
+
+public class sumOfDigits
+{
+    static void Main()
+    {
+        int n;
+        Console.WriteLine("Enter the number : ");
+        n = int.Parse(Console.ReadLine());
+
+        int rem, sum = 0;
+
+        while(n != 0)
+        {
+            rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+        Console.WriteLine("The sum of digits is " + sum);
+    }
+}
